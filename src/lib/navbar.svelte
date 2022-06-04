84005a28b3e4b968d3441c249e85151f7c33206d
Ani Dwi <anidwi.ad1@gmail.com>
2022-06-04 16:06:21 +0700

    navbar

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<nav class="navbar navbar-expand-lg py-2">
+	<div class="container-fluid">
+	  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+	  <a class="navbar-brand" href="/">
+		<img src="assets\logo.png" alt="" width="68" height="48">
+	  </a>
+	  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+		<ul class="navbar-nav fw-bolder fs-smaller mx-auto mb-2 mb-lg-0">
+		  <li class="nav-item px-2">
+			<a class="nav-link active" aria-current="page" href="/#">Home</a>
+		  </li>
+		  <li class="nav-item px-2">
+			<a class="nav-link" href="/#">About</a>
+		  </li>
+		  <li class="nav-item px-2">
+			<a class="nav-link" href="/#">Blog</a>
+		  </li>
+		</ul>
+	  </div>
+	</div>
+  </nav>
+
+<style>
+	.navbar-toggler-icon{
+		background-image: none;
+	}
+</style>
