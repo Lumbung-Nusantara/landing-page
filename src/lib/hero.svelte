4eba91e9de0d34d7667a639888baa362f73100a3
Ani Dwi <anidwi.ad1@gmail.com>
2022-06-07 18:34:23 +0700

    add hero

--- src/lib/hero.svelte
+++ src/lib/hero.svelte
@@ -1,17 +1,24 @@
-<div class="container py-5">
-    <div class="row mt-5">
-      <div class="col-10 align-self-center">
-        <h1 class="text-start">Organic Food is Good for Health</h1>
-      </div>
-    </div>
-    <div class="row mb-5">
-      <div class="col-6 align-self-center">
-        <p class="font-secondary text-start">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
-      </div>
+<script>
+    import Navbar from './navbar.svelte';
+</script>
+
+<div class="bg-hero pb-5">
+    <Navbar />
+    <div class="container py-5">
+        <div class="row mt-5">
+          <div class="col-10 align-self-center">
+            <h1 class="text-start">Organic Food is Good for Health</h1>
+          </div>
+        </div>
+        <div class="row mb-5">
+          <div class="col-6 align-self-start">
+            <p class="font-secondary text-start">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
     </div>
 </div>
 
@@ -20,13 +27,20 @@
         font-size: 5rem;
         font-weight: 400;
         line-height: 1.4;
-        max-width: 14rem;
     }
 
     p {
-        max-width: 14rem;
-        margin: 1rem auto;
+        margin: 0rem 0;
         letter-spacing: 1px;
         line-height: 1.35;
+    }
+
+    .bg-hero{
+        text-align: center;
+        margin: 0 auto;
+        background-image: url("/assets/slider1.jpg");
+        background-repeat: no-repeat;
+        background-size: 110% auto;
+        background-position: fixed;
     }
 </style>
--- src/page/landing.svelte
+++ src/page/landing.svelte
@@ -1,27 +1,11 @@
 <script>
-  import Navbar from '../lib/navbar.svelte';
+  // import Navbar from '../lib/navbar.svelte';
   import Hero from '../lib/hero.svelte';
   import About from '../lib/about.svelte';
 </script>
 
 <main>
-  <Navbar />
+  <!-- <Navbar /> -->
   <Hero />
   <About />
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    margin: 0 auto;
-    background-image: url("/assets/slider1.jpg");
-    background-repeat: no-repeat;
-    background-size: 100% auto;
-    background-position: fixed;
-  }
-</style>
