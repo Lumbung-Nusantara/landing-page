167fd0dda1f2ea29c4890eef90d6ea3ef2b96552
Ani Dwi <anidwi.ad1@gmail.com>
2022-06-07 18:12:43 +0700

    hero

---
+++ src/lib/hero.svelte
@@ -0,0 +1,32 @@
+<div class="container py-5">
+    <div class="row mt-5">
+      <div class="col-10 align-self-center">
+        <h1 class="text-start">Organic Food is Good for Health</h1>
+      </div>
+    </div>
+    <div class="row mb-5">
+      <div class="col-6 align-self-center">
+        <p class="font-secondary text-start">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+</div>
+
+<style>
+    h1 {
+        font-size: 5rem;
+        font-weight: 400;
+        line-height: 1.4;
+        max-width: 14rem;
+    }
+
+    p {
+        max-width: 14rem;
+        margin: 1rem auto;
+        letter-spacing: 1px;
+        line-height: 1.35;
+    }
+</style>
--- src/page/landing.svelte
+++ src/page/landing.svelte
@@ -4,18 +4,6 @@
 
 <main>
   <Navbar />
-    <img src='assets/logoLNnoBG.png' alt="Svelte Logo" />
-    <h1>Lumbung Nusantara</h1>
-  
-    <p>
-      Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-      apps.
-    </p>
-  
-    <p>
-      Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-      the officially supported framework, also powered by Vite!
-    </p>
 </main>
 
 <style>
@@ -31,36 +19,5 @@
     background-repeat: no-repeat;
     background-size: 100% auto;
     background-position: fixed;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
   }
 </style>
