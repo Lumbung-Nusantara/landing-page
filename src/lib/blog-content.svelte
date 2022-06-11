2e966f231fe52ca000eb1e420834dfba7ea6db80
Ani Dwi <anidwi.ad1@gmail.com>
2022-06-11 12:25:48 +0700

    blog content

--- src/lib/blog-content.svelte
+++ src/lib/blog-content.svelte
@@ -1,44 +1,46 @@
-<div class="container py-5" id="about">
-    <div class="row py-5">
-      <div class="col">
-        <img src="assets/about.png" alt="organic product">
-      </div>
-      <div class="col py-5">
-        <h1 class="text-start fw-bolder">Selamat Datang di Lumbung Nusantara</h1>
-        <h5 class="text-start my-4">WITH LOVE & DEDICATION</h5>
-        <p class="font-secondary text-muted text-start">
-          Lumbung Nusantara merupakan pioner dari sistem verifikasi dan monitoring supply chain modern berbasis blockchain Cardano yang dapat menjamin keaslian mutu produk organik favorit anda.
-        </p>
-        <button type="button" class="btn fw-bolder fs-smaller my-4">Yuk, Cari Tahu !</button>
-      </div>
-    </div>
-</div>
-  
+<section>
+  <img src="https://source.unsplash.com/1500x900/?vegetable" class="card-img-top" alt="Blog Media">
+  <div class="container">
+    <p class="text-center fw-bolder text-uppercase py-4">
+      <span>Trisha Mediana</span>
+      <span class="px-4">||</span>
+      <span>9 Juni 2022</span>
+    </p>
+    <h1 class="">
+      Lumbung Nusantara Merupakan Pioner dari Sistem Verifikasi
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+      totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. 
+      Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, 
+      sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, 
+      qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, 
+      ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, 
+      quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
+      Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, 
+      vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus, 
+      qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, 
+      obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, 
+      id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. 
+      Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, 
+      quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. 
+      Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, 
+      ut et voluptates repudiandae sint et molestiae non recusandae. 
+      Itaque earum rerum hic tenetur a sapiente delectus, 
+      ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+    </p>
+  </div>
+</section>
+
 <style>
-    img{
-      width: 80%;
-    }
-  
-    h1{
-      font-size: 3.6rem;
-      line-height: 1.4;
-    }
-  
-    h5{
-      letter-spacing: 2px;
-      color: #FAB522;
-    }
-  
-    p {
-      margin: 0rem 0;
-      letter-spacing: 1px;
-      line-height: 1.35;
-    }
-  
-    .btn{
-      color: white;
-      background-color: #FAB522;
-      padding: 1rem 2rem;
-      border-radius: 0 2rem 2rem 2rem;
-    }
+  h1 {
+    font-size: 4rem;
+    font-weight: 900;
+    line-height: 1.2;
+  }
 </style>
