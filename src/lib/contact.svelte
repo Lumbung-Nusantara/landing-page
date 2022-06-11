f69ddae1ed44c30a43644b242a94b0317bfc6cf1
Ani Dwi <anidwi.ad1@gmail.com>
2022-06-11 14:55:30 +0700

    contact

--- src/lib/contact.svelte
+++ src/lib/contact.svelte
@@ -1,114 +1,105 @@
-<div class="team-area pb-90 pos-relative" style="user-select: auto;">
-  <!-- <div class="shape sp1 bounce-animate" style="user-select: auto;"><img src="assets/sp2.png" alt="" style="user-select: auto;"></div>
-  <div class="shape sp2 bounce-animate" style="user-select: auto;"><img src="assets/sp1.png" alt="" style="user-select: auto;"></div>
-  <div class="shape sp3 bounce-animate" style="user-select: auto;"><img src="assets/sp3.png" alt="" style="user-select: auto;"></div>
-  <div class="shape sp4 bounce-animate" style="user-select: auto;"><img src="assets/sp4.png" alt="" style="user-select: auto;"></div> -->
-  <div class="container" style="user-select: auto;">
-    <div class="row" style="user-select: auto;">
-        <div class="col-xl-6 col-lg-6 offset-lg-3 offset-xl-3" style="user-select: auto;">
-          <div class="section-title text-center section-circle mb-70" style="user-select: auto;">
-            <div class="section-img" style="user-select: auto;">
-              <img src="assets/1.png" alt="" style="user-select: auto;">
-            </div>
-            <h1 style="user-select: auto;">Contact Our Teams</h1>
-            <p style="user-select: auto;">Hubungi kami dengan kontak-kontak dibawah ini </p>
+<script>
+  import { PhoneIcon, MailIcon, LinkedinIcon} from 'svelte-feather-icons';
+</script>
+
+<div class="container p-5">
+  <h1 class="text-center">Contact Our Teams</h1>
+  <p class="font-secondary text-center w-80">
+    Hubungi kami dengan kontak-kontak dibawah ini 
+  </p>
+
+  <div class="row p-5">
+    <div class="col px-5">
+      <div class="card">
+          <img src="https://source.unsplash.com/300x400/?person" class="card-img-top" alt="Blog Media">
+          <div class="font-secondary card-body py-4">
+            <h2 href="/" class="card-title text-uppercase">
+              Pablo J. Bleich 
+            </h2>
+            <p class="text-center">
+              <span class="mx-2">
+                <PhoneIcon size={"1x"}/>
+              </span>
+              <span class="mx-2">
+                <MailIcon size={"1x"}/>
+              </span>
+              <span class="mx-2">
+                <LinkedinIcon size={"1x"}/>
+              </span>
+            </p>
           </div>
-        </div>
       </div>
-      <div class="row" style="user-select: auto;">
-        <div class="col-xl-4 col-lg-4 col-md-6" style="user-select: auto;">
-          <div class="team-wrapper text-center mb-30" style="user-select: auto;">
-            <div class="team-img" style="user-select: auto;">
-              <img src="assets/dummyuser.jpg" alt="" style="user-select: auto;">
-              <div class="team-icon" style="user-select: auto;">
-                  <a href="#" style="user-select: auto;"><i class="fab fa-facebook-f" style="user-select: auto;"></i></a>
-                  <a href="#" style="user-select: auto;"><i class="fab fa-twitter" style="user-select: auto;"></i></a>
-                  <a href="#" style="user-select: auto;"><i class="fab fa-linkedin" style="user-select: auto;"></i></a>
-                  <a href="#" style="user-select: auto;"><i class="fab fa-youtube" style="user-select: auto;"></i></a>
-                  <a href="#" style="user-select: auto;"><i class="fab fa-behance" style="user-select: auto;"></i></a>
-                </div>
-            </div>
-            <div class="team-text" style="user-select: auto;">
-              <h4 style="user-select: auto;">Pablo J. Bleich</h4>
-              <span style="user-select: auto;">Organic Farmer</span>
-            </div>
+    </div>
+    <div class="col px-5">
+      <div class="card">
+          <img src="https://source.unsplash.com/300x400/?woman" class="card-img-top" alt="Blog Media">
+          <div class="font-secondary card-body py-4">
+              <h2 href="/" class="card-title text-uppercase">
+                Louise F. Shows 
+              </h2>
+              <p class="text-center">
+                <span class="mx-2">
+                  <PhoneIcon size={"1x"}/>
+                </span>
+                <span class="mx-2">
+                  <MailIcon size={"1x"}/>
+                </span>
+                <span class="mx-2">
+                  <LinkedinIcon size={"1x"}/>
+                </span>
+              </p>
           </div>
-        </div>
-        <div class="col-xl-4 col-lg-4 col-md-6" style="user-select: auto;">
-          <div class="team-wrapper text-center mb-30" style="user-select: auto;">
-            <div class="team-img" style="user-select: auto;">
-              <img src="assets/dummyuser.jpg" alt="" style="user-select: auto;">
-              <div class="team-icon" style="user-select: auto;">
-                  <a href="#" style="user-select: auto;"><i class="fab fa-facebook-f" style="user-select: auto;"></i></a>
-                  <a href="#" style="user-select: auto;"><i class="fab fa-twitter" style="user-select: auto;"></i></a>
-                  <a href="#" style="user-select: auto;"><i class="fab fa-linkedin" style="user-select: auto;"></i></a>
-                  <a href="#" style="user-select: auto;"><i class="fab fa-youtube" style="user-select: auto;"></i></a>
-                  <a href="#" style="user-select: auto;"><i class="fab fa-behance" style="user-select: auto;"></i></a>
-                </div>
-            </div>
-            <div class="team-text" style="user-select: auto;">
-              <h4 style="user-select: auto;">Louise F. Shows</h4>
-              <span style="user-select: auto;">Organic Farmer</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-xl-4 col-lg-4 col-md-6" style="user-select: auto;">
-          <div class="team-wrapper text-center mb-30" style="user-select: auto;">
-            <div class="team-img" style="user-select: auto;">
-              <img src="assets/dummyuser.jpg" alt="" style="user-select: auto;">
-              <div class="team-icon" style="user-select: auto;">
-                  <a href="#" style="user-select: auto;"><i class="fab fa-facebook-f" style="user-select: auto;"></i></a>
-                  <a href="#" style="user-select: auto;"><i class="fab fa-twitter" style="user-select: auto;"></i></a>
-                  <a href="#" style="user-select: auto;"><i class="fab fa-linkedin" style="user-select: auto;"></i></a>
-                  <a href="#" style="user-select: auto;"><i class="fab fa-youtube" style="user-select: auto;"></i></a>
-                  <a href="#" style="user-select: auto;"><i class="fab fa-behance" style="user-select: auto;"></i></a>
-                </div>
-            </div>
-            <div class="team-text" style="user-select: auto;">
-              <h4 style="user-select: auto;">Normand Shannon</h4>
-              <span style="user-select: auto;">Organic Farmer</span>
-            </div>
-          </div>
-        </div>
       </div>
+    </div>
+    <div class="col px-5">
+      <div class="card">
+          <img src="https://source.unsplash.com/300x400/?man" class="card-img-top" alt="Blog Media">
+          <div class="font-secondary card-body py-4">
+              <h2 href="/" class="card-title text-uppercase">
+                Normand Shannon
+              </h2>
+              <p class="text-center">
+                <span class="mx-2">
+                  <PhoneIcon size={"1x"}/>
+                </span>
+                <span class="mx-2">
+                  <MailIcon size={"1x"}/>
+                </span>
+                <span class="mx-2">
+                  <LinkedinIcon size={"1x"}/>
+                </span>
+              </p>
+          </div>
+      </div>
+    </div>
   </div>
 </div>
 
-  
-  <style>
-    img{
-      width: auto;
-    }
- 
-  
-    h1{
-      font-size: 3.6rem;
-      line-height: 1.4;
-    }
-  
-    h5{
-      letter-spacing: 2px;
-      color: #FAB522;
-    }
-  
-    p {
-      margin: 0rem 0;
-      letter-spacing: 1px;
-      line-height: 1.35;
-    }
-  
-    .btn{
-      color: white;
-      background-color: #FAB522;
-      padding: 1rem 2rem;
-      border-radius: 0 2rem 2rem 2rem;
-    }
-    .section-img {
-	margin-bottom: 13px;
-  width : 4 rem;
-}
-.section-img img{}
-.gambartim{
-  width : 100% ;
-}
-  </style>
+<style>
+  .container{
+      max-width: none;
+      position: relative;
+  }
+
+  h1{
+      font-size: 4rem;
+      color: #064635;
+      font-weight: 900;
+  }
+
+  h2{
+      font-size: 1.2rem;
+      font-weight: 800;
+      text-align: center;
+  }
+
+  .w-80{
+      max-width: 80%;
+      margin-left: auto;
+      margin-right: auto;
+  }
+
+  .card{
+    border: none;
+  }
+</style>
